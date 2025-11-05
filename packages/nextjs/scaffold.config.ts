3e726a0f14d398176b461ab588f9083b0aa97c38
websurferdoteth <websurfer.eth@gmail.com>
2025-11-05 14:05:16 -0800

    add env config for target chain

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -7,15 +7,18 @@ export type BaseConfig = {
   rpcOverrides?: Record<number, string>;
   walletConnectProjectId: string;
   onlyLocalBurnerWallet: boolean;
+  loginMethods: string[];
 };
 
 export type ScaffoldConfig = BaseConfig;
 
 export const DEFAULT_ALCHEMY_API_KEY = "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 
+const targetNetworks = process.env.NODE_ENV === "development" ? [chains.hardhat] : [chains.base];
+
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.base],
+  targetNetworks,
   // The interval at which your front-end polls the RPC servers for new data (it has no effect if you only target the local network (default is 4000))
   pollingInterval: 30000,
   // This is ours Alchemy's default API key.
@@ -35,6 +38,7 @@ const scaffoldConfig = {
   // .env.local for local testing, and in the Vercel/system env config for live apps.
   walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "3a8170812b534d0ff9d794f19a901d64",
   onlyLocalBurnerWallet: true,
+  loginMethods: ["email", "sms", "wallet", "google", "twitter"],
 } as const satisfies ScaffoldConfig;
 
 export default scaffoldConfig;
